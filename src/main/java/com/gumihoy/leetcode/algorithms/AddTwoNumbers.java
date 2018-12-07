f008e454299e2756b44eb294d86b9587ea562a4c
kongtong.oykt <kongtong.oykt@alibaba-inc.com>
2018-12-07 21:25:33 +0800

    Two Sum

---
+++ src/main/java/com/gumihoy/leetcode/algorithms/AddTwoNumbers.java
@@ -0,0 +1,34 @@
+package com.gumihoy.leetcode.algorithms;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author kongtong.ouyang on 2018/12/7.
+ */
+public class AddTwoNumbers {
+
+
+    @Test
+    public void testAddTwoNumbers() {
+        ListNode l1 = null;
+        ListNode l2 = null;
+        addTwoNumbers(l1, l2);
+    }
+
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        return null;
+    }
+
+    public class ListNode {
+        protected int val;
+        protected ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
---
+++ src/main/java/com/gumihoy/leetcode/algorithms/TwoSum.java
@@ -0,0 +1,43 @@
+package com.gumihoy.leetcode.algorithms;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ * @author kongtong.ouyang on 2018/12/7.
+ */
+public class TwoSum {
+
+    @Test
+    public void testTwoSum() {
+        int[] nums = {2, 7, 11};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0] + ", " + result[1]);
+        Assert.assertEquals(0, result[0]);
+        Assert.assertEquals(1, result[1]);
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+}
